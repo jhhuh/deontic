7397f5efe9f5fb389dc848c1aa0591e944467f14
Ji-Haeng Huh <jhhuh.note@gmail.com>
2026-03-07 04:18:18 +0900

    feat(kr-civil): add MinorAct and JuristicAct types

--- deontic-kr-civil/src/Deontic/Civil/Types.hs
+++ deontic-kr-civil/src/Deontic/Civil/Types.hs
@@ -1,1 +1,18 @@
-module Deontic.Civil.Types where
+module Deontic.Civil.Types
+  ( MinorAct(..)
+  , JuristicAct(..)
+  ) where
+
+import Deontic.Core.Types (PersonId, ActId)
+
+-- | 미성년자의 법률행위 (민법 제5조)
+data MinorAct = MinorAct
+  { maActor :: PersonId
+  , maActId :: ActId
+  } deriving (Eq, Show)
+
+-- | 일반 법률행위 (민법 제103조-제107조)
+data JuristicAct = JuristicAct
+  { jaActor :: PersonId
+  , jaActId :: ActId
+  } deriving (Eq, Show)
