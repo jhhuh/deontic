95ae4bfb0e60e06de0cc4dc557563aa337910ff7
Ji-Haeng Huh <jhhuh.note@gmail.com>
2026-03-07 00:52:16 +0900

    feat(core): add LegalCode typeclass and query interface

---
+++ deontic-core/test/Deontic/QuerySpec.hs
@@ -0,0 +1,28 @@
+{-# LANGUAGE OverloadedStrings #-}
+module Deontic.QuerySpec (spec) where
+
+import Test.Hspec
+import qualified Data.Set as Set
+import Deontic.Core.Types
+import Deontic.Core.Judgment
+import Deontic.Defeasible.Rule
+import Deontic.Query
+
+spec :: Spec
+spec = do
+  describe "LegalCode typeclass" $ do
+    it "can query a simple code" $ do
+      let art5_1 = ArticleRef "민법" 5 (Just 1)
+          minor = PersonId "김철수"
+          code = SimpleCode
+            [ Rule
+                { ruleId = art5_1
+                , precondition = \fs -> IsMinor minor `Set.member` fs
+                , conclusion = JVoidable [art5_1]
+                , defeatedBy = []
+                }
+            ]
+          facts = Set.fromList [IsMinor minor]
+          result = query code facts
+      length result `shouldBe` 1
+      fst (head result) `shouldBe` JVoidable [art5_1]
