dec9b9f7ff67ac06a65fa08f7e4e5702d933b33d
Ji-Haeng Huh <jhhuh.note@gmail.com>
2026-03-07 00:45:02 +0900

    feat(core): add Judgment and ProofTree types

--- deontic-core/src/Deontic/Core/Judgment.hs
+++ deontic-core/src/Deontic/Core/Judgment.hs
@@ -1,1 +1,45 @@
-module Deontic.Core.Judgment where
+module Deontic.Core.Judgment
+  ( Judgment(..)
+  , isValid, isVoid, isVoidable
+  , citations
+  , ProofTree(..)
+  , rootArticle
+  ) where
+
+import Deontic.Core.Types (ArticleRef)
+
+data Judgment
+  = JValid    [ArticleRef]
+  | JVoid     [ArticleRef]
+  | JVoidable [ArticleRef]
+  | JAmbiguous [(Judgment, [ArticleRef])]
+  deriving (Eq, Show)
+
+isValid :: Judgment -> Bool
+isValid (JValid _) = True
+isValid _          = False
+
+isVoid :: Judgment -> Bool
+isVoid (JVoid _) = True
+isVoid _         = False
+
+isVoidable :: Judgment -> Bool
+isVoidable (JVoidable _) = True
+isVoidable _             = False
+
+citations :: Judgment -> [ArticleRef]
+citations (JValid cs)     = cs
+citations (JVoid cs)      = cs
+citations (JVoidable cs)  = cs
+citations (JAmbiguous js) = concatMap snd js
+
+data ProofTree
+  = Leaf ArticleRef
+  | Defeated ArticleRef [ArticleRef]
+  | Derived ArticleRef [ProofTree]
+  deriving (Eq, Show)
+
+rootArticle :: ProofTree -> ArticleRef
+rootArticle (Leaf a)       = a
+rootArticle (Defeated a _) = a
+rootArticle (Derived a _)  = a
